c956f623acc9256c19043862843d96b5aa93604e
Rahul Kumar <kumarrahulraj3235@gmail.com>
2026-06-01 19:28:22 +0530

    first commit

---
+++ README.md/main.py
@@ -0,0 +1,9 @@
+# Find the largest of three numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+largest = max(a, b, c)
+
+print("Largest number is:", largest)
